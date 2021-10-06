ddc0e3a2ce5f6db2fb604372eae62aa065416180
LakshmanKishore <lakshmankishore17.com>
2021-10-06 15:17:54 +0530

    Fix deployment

--- app.py
+++ app.py
@@ -13,35 +13,33 @@ def Home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    data=dict(request.form)
-    print(data)
+    data=request.form.to_dict()
 
-    Gender = 1 if data["Gender"][0]=="male" else 0
-    Married = 1 if data["Married"][0]=="yes" else 0
-    if data["Dependents"][0]=="0":
+    Gender = 1 if data["Gender"]=="male" else 0
+    Married = 1 if data["Married"]=="yes" else 0
+    if data["Dependents"]=="0":
         Dependents = 0
-    elif data["Dependents"][0]=="1":
+    elif data["Dependents"]=="1":
         Dependents = 1
-    elif data["Dependents"][0]=="2":
+    elif data["Dependents"]=="2":
         Dependents = 2
     else:
         Dependents = 3
-    Education = 0 if data["Education"][0]=="Graduate" else 1
-    Self_Employed = 1 if data["Self_Employed"][0]=="s_yes" else 0
-    LoanAmount = np.log(int(data["LoanAmount"][0]))
-    Loan_Amount_Term = np.log(int(data["Loan_Amount_Term"][0]))
-    Credit_History = 1 if data["Credit_History"][0]=="c_yes" else 0
-    if data["Property_Area"][0]=="Rural":
+    Education = 0 if data["Education"]=="Graduate" else 1
+    Self_Employed = 1 if data["Self_Employed"]=="s_yes" else 0
+    LoanAmount = np.log(int(data["LoanAmount"]))
+    Loan_Amount_Term = np.log(int(data["Loan_Amount_Term"]))
+    Credit_History = 1 if data["Credit_History"]=="c_yes" else 0
+    if data["Property_Area"]=="Rural":
         Property_Area = 0
-    elif data["Property_Area"][0]=="Semiurban":
+    elif data["Property_Area"]=="Semiurban":
         Property_Area = 1
     else:
         Property_Area = 2
-    TotalIncome = np.log(int(data["ApplicantIncome"][0])+int(data["CoapplicantIncome"][0]))
+    TotalIncome = np.log(int(data["ApplicantIncome"])+int(data["CoapplicantIncome"]))
 
     predictionData = [Gender,Married,Dependents,Education,Self_Employed,LoanAmount,Loan_Amount_Term,Credit_History,Property_Area,TotalIncome]
     result = model.predict([predictionData])
-    print(predictionData,result)
     if result[0]==1:
         result = "will"
     else:
